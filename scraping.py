d3192fdbdd58c7773f75690e8a37907e6a73a800
sganhewage <sganhewage@guilford.edu>
2024-07-29 15:37:13 -0400

    login process works

--- scraping.py
+++ scraping.py
@@ -17,7 +17,22 @@ def scraping() -> None:
     #Ensure correct website
     assert "Amazon" in driver.title
 
+    #CAPTCHA
+    captcha = True
+    try:
+        text = driver.find_element(By.XPATH, "//h1[@class = 'a-spacing-small']").text
+        if (text == "Sign in"): captcha = False
+    except:
+        pass
+    if (captcha):
+        captchaImageLink = driver.find_element(By.XPATH, "//div[@class = 'a-row a-text-center']//img").get_attribute('src')
+        capthca_value = (AmazonCaptcha.fromlink(captchaImageLink)).solve()
+        elem = driver.find_element(By.XPATH, "//div[@class = 'a-row a-spacing-base']//input")
+        elem.send_keys(capthca_value)
+        elem.send_keys(Keys.RETURN)
+
     #Enter email
+    driver.get("https://www.amazon.com/gp/sign-in.html")
     elem = driver.find_element(By.ID, "ap_email")
     elem.send_keys(email)
     elem.send_keys(Keys.RETURN)
@@ -30,36 +45,19 @@ def scraping() -> None:
     elem.send_keys(password)
     elem.send_keys(Keys.RETURN)
 
-    # # #CAPTCHA
-    # #Wait for login to complete
-    # driver.implicitly_wait(5)
-    # captchaImageLink = driver.find_element(By.XPATH, "//div[@class = 'a-section a-text-center cvf-captcha-img']//img").get_attribute('src')
-    # capthca_value = (AmazonCaptcha.fromlink(captchaImageLink)).solve()
-    # print(capthca_value)
-
-    # elem = driver.find_element(By.XPATH, "//div[@class = 'a-row a-spacing-base']//input")
-    # elem.send_keys(capthca_value)
-    # elem.send_keys(Keys.RETURN)
-
     #Go to orders and select current year
     ordersButton = driver.find_element(By.ID, "nav-orders")
     ordersButton.click()
-
-    # driver.implicitly_wait(5)
-    # elem = driver.find_element(By.ID, "a-autoid-1-announce")
-    # elem.click()
-
-    # driver.implicitly_wait(5)
-    # elem = driver.find_element(By.ID, "time-filter_2")
-    # elem.click()
-
-    # year: int = int(driver.find_element(By.ID, "a-autoid-1-announce").find_element(By.CLASS_NAME, "a-dropdown-prompt").text)
-    # dropdown_option = 3
 
     year: int = int(datetime.datetime.now().year)
     dropdown_option = 2
     #Get all orders
     orders = driver.find_elements(By.XPATH, "//div[@class = 'order-card js-order-card']")
+    for order in orders:
+        order.find_element(By.XPATH, "//span[@class = 'a-size-base a-color-secondary']")
+        print(order.get_attribute('span'))
+        # print(order.find_element(By.XPATH, "//div[@class = 'a-column a-span3']").text)
+
     while year >= (start_year+1):
         driver.implicitly_wait(5)
         elem = driver.find_element(By.ID, "a-autoid-1-announce")

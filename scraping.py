2284dbd3f15a5e7c276e2ca6b3dbad41bed196ec
sganhewage <sganhewage@guilford.edu>
2024-08-05 03:33:58 -0400

    full item iteration working

--- scraping.py
+++ scraping.py
@@ -9,7 +9,7 @@ from orderclass import Order
 def scraping() -> None:
     email: str = "sganhewage1@gmail.com"
     password: str = input("Input Password: ")
-    start_year: int = 2022
+    start_year: int = 2024
 
     driver = webdriver.Firefox()
     driver.get("https://www.amazon.com/gp/sign-in.html")
@@ -51,15 +51,11 @@ def scraping() -> None:
 
     year: int = int(datetime.datetime.now().year)
     dropdown_option = 2
-    #Get all orders
-    orders = driver.find_elements(By.XPATH, "//div[@class = 'order-card js-order-card']")
-    for order in orders:
-        order.find_element(By.XPATH, "//span[@class = 'a-size-base a-color-secondary']")
-        print(order.get_attribute('span'))
-        # print(order.find_element(By.XPATH, "//div[@class = 'a-column a-span3']").text)
 
-    while year >= (start_year+1):
-        driver.implicitly_wait(5)
+    # Get all orders
+    orders = []
+    while year >= (start_year):
+        driver.implicitly_wait(5)            
         elem = driver.find_element(By.ID, "a-autoid-1-announce")
         elem.click()
 
@@ -67,14 +63,53 @@ def scraping() -> None:
         driver.implicitly_wait(5)
         elem = driver.find_element(By.ID, buttonID)
         elem.click()
+        
+        driver.implicitly_wait(5)
+        numPages = driver.find_elements(By.CLASS_NAME, "a-normal").__len__() + 1
+        print(numPages)
+        for i in range(numPages):
+            orderElements = driver.find_elements(By.XPATH, "//div[@class = 'order-card js-order-card']")
+            for order in orderElements:
+                orders.append(str(order.find_element(By.CLASS_NAME, "a-link-normal").get_attribute('href')))
+            nextButton = driver.find_element(By.CLASS_NAME, "a-last")
+            nextButton.click()
+            driver.implicitly_wait(5)
 
-        year: int = int(driver.find_element(By.ID, "a-autoid-1-announce").find_element(By.CLASS_NAME, "a-dropdown-prompt").text)
+        year: int = int(driver.find_element(By.ID, "a-autoid-1-announce").find_element(By.CLASS_NAME, "a-dropdown-prompt").text)-1
         dropdown_option += 1
 
-        new_orders = driver.find_elements(By.XPATH, "//div[@class = 'order-card js-order-card']")
-        orders += new_orders
-
+    orders = list(set(orders))
     print(orders.__len__())
+    
+    # Grab Order Information
+    for order in orders:
+        driver.get(order)
+        driver.implicitly_wait(5)
+        
+        # Order Number
+        orderNumber = driver.find_element(By.XPATH, "//span[@class = 'order-date-invoice-item']//bdi").text
+        print(orderNumber)
+        
+        # Order Date
+        orderDate = driver.find_element(By.XPATH, "//span[@class = 'order-date-invoice-item']").text
+        orderDate = orderDate[11:]
+        print(orderDate)
+        
+        # Loop following attributes
+        
+        # Total Before Tax
+        totalBeforeTax = driver.find_element(By.XPATH, "//span[@class = 'a-size-small a-color-price']").text
+        print(totalBeforeTax)
+        
+        # Item Name
+        itemName = driver.find_element(By.XPATH, "//div[@class = 'a-fixed-left-grid-inner']//div[@class = 'a-row']//a[@class = 'a-link-normal']").text
+        print(itemName)
+        
+        # Seller
+        seller = driver.find_element(By.XPATH, "//div[@class = 'a-fixed-left-grid-inner']//span[@class = 'a-size-small a-color-secondary']//a[@class = 'a-link-normal']").text
+        print(seller)
+        # Delivery Date
+        
 
 if __name__ == "__main__":
     scraping()
